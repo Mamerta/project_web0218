a3dc4060d2ffc40810944c2847d3cb1054b1a6d8
Mamerta <48089154+Mamerta@users.noreply.github.com>
2019-03-16 22:09:07 +0200

    Add 03-16
    Add show less block

--- assets/scripts/custom.js
+++ assets/scripts/custom.js
@@ -1,3 +1,18 @@
 "use strict";
 
-console.log("JS Veikia!");
+// Image gallery burger - show more, show less
+
+$(document).ready(function(){
+	$('.gallery-btn').click(function(e){
+		$('.hidden').toggleClass('show');
+		$('.show1').toggleClass('hidden1');
+			e.preventDefault();
+	});
+});
+
+
+// $(document).ready(function(){
+// 				$('.click-hide').click(function(){
+// 					this.remove();	
+// 				});
+// });
